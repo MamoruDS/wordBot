eec696c3e7c133be1e9e3d70d9f00993f1e6a5c7
MamoruDS <mamoruds.io@gmail.com>
2020-02-15 22:04:57 +0900

    fixed request methods structure problem

--- src/request.ts
+++ src/request.ts
@@ -2,14 +2,23 @@ import axios from 'axios'
 
 import * as types from './types'
 
-export const getBook = async (bookId?: string): Promise<types.bookObj[]> => {
-    const res = await axios({
-        baseURL: this._server.url,
-        url: '/api/book',
-        method: 'get',
-        params: {
-            book_id: bookId,
-        },
-    })
-    return res.data.data
+class request {
+    private _serverUrl: string
+    constructor(serverUrl) {
+        this._serverUrl = serverUrl
+    }
+
+    getBook = async (bookId?: string): Promise<types.bookObj[]> => {
+        const res = await axios({
+            baseURL: this._serverUrl,
+            url: '/api/book',
+            method: 'get',
+            params: {
+                book_id: bookId,
+            },
+        })
+        return res.data.data
+    }
 }
+
+export = request
