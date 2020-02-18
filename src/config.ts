6bd0b2d8746fbfa5c44a69a4464409d625d25f11
MamoruDS <mamoruds.io@gmail.com>
2020-02-18 10:40:43 +0900

    added _.get<> methods

--- src/config.ts
+++ src/config.ts
@@ -7,3 +7,36 @@ const CONFIG_PATH = `./config.yml`
 const CONFIG_PATH_DEFAULT = `./static/_config.yml`
 export const LANG = `./static/locales/`
 export const MENU = `./static/inlineMenu/`
+
+if (!fs.existsSync(CONFIG_PATH)) {
+    fs.copyFileSync(CONFIG_PATH_DEFAULT, CONFIG_PATH)
+}
+
+const getProfile = (): types.profile => {
+    let _profile = {} as types.profile
+    try {
+        _profile = yaml.safeLoad(fs.readFileSync(CONFIG_PATH, 'utf-8'))
+    } catch (e) {
+        // if(e instanceof yaml.YAMLException)
+        console.log('[ERROR] Can not parse yaml config file.')
+        process.exit(1)
+    } finally {
+        return _profile
+    }
+}
+
+export const getServer = (): types.serverInfo => {
+    return getProfile().server
+}
+
+export const getBot = (): { token: string; owner_id?: string | number } => {
+    return getProfile().bot
+}
+
+export const getLang = (): string => {
+    return getProfile().lang
+}
+
+export const getLineWidth = (): number => {
+    return getProfile()['line-width']
+}
